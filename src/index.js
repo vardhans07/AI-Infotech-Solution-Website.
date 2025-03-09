56867c6b96a78e2627807ba823b92a4ea95e3c38
RAJVARDHAN PATIL <95933896+vardhans07@users.noreply.github.com>
2025-03-09 20:34:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles.css'; // Ensure this path is correct
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
