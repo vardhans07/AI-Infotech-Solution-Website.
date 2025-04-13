19c85ddcb0d47d51723339cfa604500709d04316
RAJVARDHAN PATIL <95933896+vardhans07@users.noreply.github.com>
2025-04-13 21:00:51 +0530

    Add files via upload

---
+++ src/heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Heading({ subtitle, title }) {
+    return (
+        <div id="heading">
+            <h3>{subtitle}</h3>
+            <h1>{title}</h1>
+        </div>
+    );
+}
+
+export default Heading;
